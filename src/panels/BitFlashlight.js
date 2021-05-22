c91c192e6b9c6032e0ca1fc94f848c7f8b70a26e
Алексей <alexleskov2000@gmail.com>
2021-05-22 20:26:40 +0800

    Использование класса

--- src/panels/BitFlashlight.js
+++ src/panels/BitFlashlight.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
     Group,
     Div,
@@ -5,23 +6,50 @@ import {
     PanelHeader,
     View, PanelHeaderContent
 } from "@vkontakte/vkui";
+import bridge from "@vkontakte/vk-bridge";
 
-export default function TeamCard() {
-    return (
-        <View activePanel="main">
-            <Panel id="main">
-                <PanelHeader>
-                    BitFlashlight
-                    <PanelHeaderContent
-                        status={"Created by NaniB0ots"}>
-                    </PanelHeaderContent>
-                </PanelHeader>
 
-                <Group>
-                    <Div>NaniB0ots</Div>
-                </Group>
+class TeamCard extends React.Component{
+    init() {
+        console.log('asd!!')
+        bridge.send("VKWebAppShowWallPostBox", {"message": "Hello!"}).then(
+            data => console.log(data)
+        ).catch(
+            error => console.log(error)
+        );
+        bridge.send("VKWebAppShowOrderBox", {type: "item", item: "item_id_123"});
+        bridge.subscribe((e) => {
+                if (e.type == 'VKWebAppShowOrderBoxResult') {
+                    console.log(e.data.status);
+                }
+            }
+        );
+    }
 
-            </Panel>
-        </View>
-    )
+    render() {
+        return (
+
+            <View activePanel="main">
+                <Panel id="main">
+                    <PanelHeader>
+                        BitFlashlight
+                        <PanelHeaderContent
+                            status={
+                                "Created by NaniB0ots"
+                            }
+
+                        >
+                        </PanelHeaderContent>
+                    </PanelHeader>
+
+                    <Group>
+                        <Div>NaniB0ots</Div>
+                    </Group>
+
+                </Panel>
+            </View>
+        )
+    }
 }
+
+export default TeamCard;
