99f0f23891407e2a1eb8aa9e879df547917e0b6b
Алексей <alexleskov2000@gmail.com>
2021-05-22 20:03:01 +0800

    Добавить PanelHeader

--- src/panels/BitFlashlight.js
+++ src/panels/BitFlashlight.js
@@ -1,5 +1,27 @@
+import {
+    Group,
+    Div,
+    Panel,
+    PanelHeader,
+    View, PanelHeaderContent
+} from "@vkontakte/vkui";
+
 export default function TeamCard() {
     return (
-        <div>asd</div>
+        <View activePanel="main">
+            <Panel id="main">
+                <PanelHeader>
+                    BitFlashlight
+                    <PanelHeaderContent
+                        status={"Created by NaniB0ots"}>
+                    </PanelHeaderContent>
+                </PanelHeader>
+
+                <Group>
+                    <Div>NaniB0ots</Div>
+                </Group>
+
+            </Panel>
+        </View>
     )
 }
